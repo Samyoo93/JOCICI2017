734af04c777df2bc5b42df48c3fcc0f2b04b474d
Samuel <green.life93@gmail.com>
2017-03-09 21:31:49 -0600

    1.4.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.scss','*.js.coffee']
+  config.assets.precompile =  ['*.js', '*.css', '*.css.scss']
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
