59c35081648d3e2a4ad9f393d38754183b90151c
Samuel <green.life93@gmail.com>
2017-03-09 21:29:12 -0600

    1.4.2
    internal issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.scss','*.js.coffee']
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
